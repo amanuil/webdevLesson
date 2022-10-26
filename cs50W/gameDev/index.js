54c35baef31111cdf13a1f3808d4f085295324a0
amanuil <81981466+amanuil@users.noreply.github.com>
2022-10-26 08:55:27 +0300

    Add files via upload
    added CS50W folder

---
+++ cs50W/gameDev/index.js
@@ -0,0 +1,4 @@
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext("2d");
+
+ctx.clearRect(0, 0, 800, 600);
---
+++ cs50W/gameDev/paddle.js
@@ -0,0 +1,9 @@
+class Paddle {
+    constructor() {
+
+    }
+
+    draw(ctx){
+        
+    }
+}
